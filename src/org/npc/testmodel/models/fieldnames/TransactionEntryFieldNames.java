351c185b65e6f44c8e251b56d70d7a75f9f80052
rhgraham <rhgraham1130@gmail.com>
2015-09-28 11:03:32 -0500

    Changed class declaration to match file name
    Ctrl+C/Ctrl+V strikes again :)

--- src/org/npc/testmodel/models/fieldnames/TransactionEntryFieldNames.java
+++ src/org/npc/testmodel/models/fieldnames/TransactionEntryFieldNames.java
@@ -2,7 +2,7 @@ package org.npc.testmodel.models.fieldnames;
 
 import org.npc.dataaccess.model.BaseFieldNames;
 
-public class ProductFieldNames extends BaseFieldNames {
+public class TransactionEntryFieldNames extends BaseFieldNames {
 	public static final String RECORD_ID = "recordid";
 	public static final String TRANSACTION_ID = "transactionid";
 	public static final String PRODUCT_ID = "productid";
