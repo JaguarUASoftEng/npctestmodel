110d9de0d625d5bed1bb7b3cc4e7a1e01858042c
rhgraham <rhgraham1130@gmail.com>
2015-10-09 22:19:48 -0500

    Added field name constants for TenderEntry

---
+++ src/org/npc/testmodel/models/fieldnames/TenderEntryFieldNames.java
@@ -0,0 +1,12 @@
+package org.npc.testmodel.models.fieldnames;
+
+import org.npc.dataaccess.model.BaseFieldNames;
+
+public class TenderEntryFieldNames extends BaseFieldNames
+{
+	public static final String ID = "id";
+	public static final String TRANSACTION_ID = "transaction_id";
+	public static final String TENDER_TYPE = "tender_type";
+	public static final String AMOUNT = "amount";
+	public static final String CREATED_ON = "createdon";
+}
