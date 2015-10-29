13c66f164009c753417db2e50eadc72e888087d2
bryan-janes <bryan.a.janes@gmail.com>
2015-10-29 13:14:52 -0500

    Initial Transaction Model

---
+++ src/org/npc/testmodel/models/Transaction.java
@@ -0,0 +1,96 @@
+package org.testmodel.api;
+
+import java.util.UUID;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.apache.commons.lang3.StringUtils;
+import org.npc.testmodel.enums.ProductApiRequestStatus;
+
+@XmlRootElement
+public class Transaction
+{
+	protected UUID r_id;
+	private int c_id;
+	private int amount;
+	private String transactiontype;
+	//not sure how to add timestamp for created on
+	
+		public UUID getR_ID()
+	{
+		return this.r_id;
+	}
+
+	public Transaction setR_ID(UUID r_id)
+	{
+		this.r_id = r_id;
+		return this;
+	}
+	
+	public int getC_ID()
+	{
+		return this.c_id;
+	}
+
+	public Transaction setC_ID(int c_id)
+	{
+		if (this.c_id != c_id)
+		{
+			this.c_id = c_id;
+		}
+		return this;
+	}
+	
+	public int getAmount()
+	{
+		return this.amount;
+	}
+
+	public Transaction setAmount(int amount)
+	{
+		if (this.amount != amount)
+		{
+			this.amount = amount;
+		}
+		return this;
+	}
+	
+	public String getTransactonType()
+	{
+		return this.transactiontype;
+	}
+
+	public Transaction setTransactionType(String transactiontype)
+	{
+		if (!StringUtils.equals(this.transactiontype, transactiontype))
+		{
+			this.transactiontype = transactiontype;
+		}
+		return this;
+	}
+	
+	public Transaction setApiRequestMessage(String apiRequestMessage)
+	{
+		if (!StringUtils.equalsIgnoreCase(this.apiRequestMessage, apiRequestMessage))
+		{
+			this.apiRequestMessage = apiRequestMessage;
+		}
+		return this;
+	}	
+	
+	public Transaction()
+	{
+		this.r_id = UUID.randomUUID();
+		this.c_id = -1;
+		this.amount = -1;
+		this.transactiontype = StringUtils.EMPTY:
+	}
+	
+	public Transaction(org.npc.testmodel.models.Transaction modelTransaction)
+	{
+		this.r_id = modelTransaction.getR_ID();
+		this.c_id = modelTransaction.getC_ID();
+		this.amount = modelTransaction.getAmount();
+		this.transactiontype = modelTransaction.getTransactonType();
+		this.apiRequestMessage = StringUtils.EMPTY;
+		this.apiRequestStatus = ProductApiRequestStatus.OK;
+	}
+}
